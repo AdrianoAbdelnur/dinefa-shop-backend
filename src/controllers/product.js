30479230f21bfbe049e5e18332910a7a4be0e8a4
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-11-16 10:25:26 -0300

    Correction in message

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -6,7 +6,7 @@ const addProduct = async (req, res) => {
         const productToAdd = req.body; 
         const newProduct = new Product(productToAdd);
         await newProduct.save();
-        res.status(200).json({ message: 'Producto creada con exito', newProduct })
+        res.status(200).json({ message: 'Producto añadido exitosamente', newProduct })
     } catch (error) {
         res.status(error.code || 500).json({ message: error.message });
     }
@@ -14,7 +14,7 @@ const addProduct = async (req, res) => {
 
 const getAllProducts= async (req, res) => {
     try {
-        const productsFound  = await Product.find( { isDeleted: false }).populate('category');
+        const productsFound  = await Product.find({ isDeleted: false }).populate('category');
         if (productsFound.length === 0) return res.status(400).json({ message: 'lista de productos vacia' });
         return res.status(200).json({ message: 'Productos obtenidos de forma exitosa', products: productsFound })
     } catch (error) {
